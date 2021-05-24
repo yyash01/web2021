3a4a8fbaffd06f9f2460d9fc101ad3ab3dc6edf1
yyash01 <yjain3.436@gmail.com>
2021-05-24 13:47:08 +0530

    A file which shows the result for creating ToDoitem- and cutting onthrough line while clicking on it

---
+++ React-practice/learn14/ToDoitem.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function ToDoitem(props) {
+    const [isDone, setIsDone] = useState(false);
+
+    function handleClick() {
+        setIsDone((prevValue) => {
+            return !prevValue;
+        });
+    }
+
+    return (
+        <div onClick={handleClick}>
+            <li style={{ textDecoration: isDone ? "line-through" : "none" }}>{props.text}</li>
+        </div>
+    );
+}
+
+export default ToDoitem;
